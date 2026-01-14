9a7ede569c894d38c6bd218486023e8017c6a2f7
LvDias <84280349+LvDias@users.noreply.github.com>
2026-01-14 12:59:39 -0300

    adds a new method for adding SCK codes to purchase links

--- app/@components/button.tsx
+++ app/@components/button.tsx
@@ -1,10 +1,17 @@
+import { cookies } from "next/headers"
 import Link from "next/link"
 
-export function Button({ children }: { children: React.ReactNode }) {
+export async function Button({ children }: { children: React.ReactNode }) {
+    const cookieStore = await cookies()
+    const lead = cookieStore.get("lead")?.value ?? "cookieLead"
+
+    const url = new URL("https://pay.cakto.com.br/6kug9q3_721219")
+    url.searchParams.set("sck", lead)
+
     return (
         <Link
             className="flex items-center justify-center gap-2 rounded bg-blue-600 px-4 py-2 font-bold text-white text-xs uppercase xl:text-sm"
-            href="https://pay.cakto.com.br/6kug9q3_721219"
+            href={url.toString()}
         >
             {children}
         </Link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -40,14 +40,6 @@ export default function RootLayout({
                 <Footer />
 
                 <Script
-                    id="gtm-params-link-checkout"
-                    // biome-ignore lint/security/noDangerouslySetInnerHtml: Script Checkout Link With Params
-                    dangerouslySetInnerHTML={{
-                        __html: `let prefix=["https://pay.cakto.com.br"];function getParams(){let t="",e=window.top.location.href,r=new URL(e);if(null!=r){let a=r.searchParams.get("utm_source"),n=r.searchParams.get("utm_medium"),o=r.searchParams.get("utm_campaign"),m=r.searchParams.get("utm_term"),p=r.searchParams.get("fbclid"),c=r.searchParams.get("utm_content");-1!==e.indexOf("?")&&(t="&src="+a+"|"+n+"|"+o+"|"+m+"|"+c);console.log(t)}return t}function getCookie(name){let match=document.cookie.match(new RegExp("(^| )"+name+"=([^;]+)"));if(match){return match[2]}return null}function updateLinks(){var t=new URLSearchParams(window.location.search);var sck=getCookie("lead");if(sck){t.append("sck",sck);t.toString()&&document.querySelectorAll("a").forEach(function(e){for(let r=0;r<prefix.length;r++){if(-1!==e.href.indexOf(prefix[r])){if(-1===e.href.indexOf("?")){e.href+="?"+t.toString()+getParams()}else{e.href+="&"+t.toString()+getParams()}}}})}else{setTimeout(updateLinks,100)}}window.addEventListener("load",function(){updateLinks()});`,
-                    }}
-                />
-
-                <Script
                     id="gtm-stape-init"
                     strategy="afterInteractive"
                     // biome-ignore lint/security/noDangerouslySetInnerHtml: Script GTM Server Side
